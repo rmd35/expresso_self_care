f7888bcd43a0f180b4770d355efe4da215be78d1
Rola <144335560+rmd35@users.noreply.github.com>
2025-07-20 23:24:50 +0300

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,19 @@
 import 'dart:ui';
 
+import 'package:google_fonts/google_fonts.dart';
+
 const kPrimaryColor = Color(0xffFFD218);
+const kTitleTextColor = Color(0xFF472682);
+const kSubtitleTextColor = Color(0xFF432C7F);
+
+final kTitleTextStyle = GoogleFonts.montserrat(
+  color: kTitleTextColor,
+  fontSize: 30,
+  fontWeight: FontWeight.bold,
+);
+
+final kSubtitleTextStyle = GoogleFonts.montserrat(
+  color: kSubtitleTextColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w300,
+);
