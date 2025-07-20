68686d9b076ad36aea18aa38039fffaf368a07b2
Rola <144335560+rmd35@users.noreply.github.com>
2025-07-20 23:24:15 +0300

    Update choose_language_view.dart

--- lib/ui/views/choose_language/choose_language_view.dart
+++ lib/ui/views/choose_language/choose_language_view.dart
@@ -1,4 +1,6 @@
+import 'package:expresso_self_care/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChooseLanguageView extends StatelessWidget {
   const ChooseLanguageView({super.key});
@@ -6,24 +8,46 @@ class ChooseLanguageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/background.png',
                 fit: BoxFit.cover,
               ),
             ),
-          ),
-          Column(
-            children: <Widget>[
-              Text('LANGUAGE'),
-              SizedBox(height: 20),
-              Text('Choisissez votre langue'),
-            ],
-          ),
-        ],
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('LANGUAGE', style: kTitleTextStyle),
+                  SizedBox(height: 20),
+                  Text('Choisissez votre langue', style: kSubtitleTextStyle),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(26.5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          SvgPicture.asset('assets/svg/english.svg'),
+                          Text(
+                            'ENGLISH',
+                            style: kSubtitleTextStyle.copyWith(fontSize: 19),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
